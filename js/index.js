7414ad0edfbf1205a8ab35d7e0b184f17354202c
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-10-23 14:46:31 -0700

    added npm packages

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-const heroButton = document.querySelector(".hero-button")
+AOS.init();
+
+const heroButton = document.querySelector(".hero-button");
+const venture = document.querySelectorAll(".venture-link");
+
+function navigateTo(query) {
+    const el = document.querySelector(query);
+    el.scrollIntoView({ behavior: "smooth", block: "start" });
+};
 
 heroButton.addEventListener("mouseenter", () => {
     heroButton.classList.add('hero-button-hover');
@@ -7,3 +15,13 @@ heroButton.addEventListener("mouseenter", () => {
 heroButton.addEventListener("mouseleave", () => {
     heroButton.classList.remove('hero-button-hover');
 }, false);
+
+for (link of venture) {
+    link.addEventListener("mousedown", () => {
+        navigateTo("article.venturing")
+    })
+}
+
+// heroButton.addEventListener("mousedown", () => {
+//     navigateTo("article.main-content");
+// })
