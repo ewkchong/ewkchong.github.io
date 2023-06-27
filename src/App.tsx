72d7009b2bde26af8038f9fd508839ea50de566d
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2023-06-26 17:25:11 -0700

    finalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import NavBar from './components/Navbar'
-import ProjectList from './components/ProjectList'
+// import ProjectList from './components/ProjectList'
 
 function App() {
 
