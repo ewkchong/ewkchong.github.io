b7389e4e0b81433b4edecff86e55843cac78742a
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2023-06-26 17:54:28 -0700

    add email hover effect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,10 @@
 import './css/Hero.css'
 import Button from './Button.tsx';
+import { useState } from 'react';
 
 const Hero = () => {
+
+	const [emailHover, setEmailHover] = useState(false);
 
 	/* function handleClick() {
 		const projects = document.getElementById("projects-header");
@@ -15,7 +18,15 @@ const Hero = () => {
 			<h1>I'm Edward.</h1>
 			<p>Computer Science and Finance nerd.</p>
 
-			<Button text="Email" href="mailto:eddiewchong@outlook.com" />
+			<a className="hero-link" href="mailto:eddiewchong@outlook.com" target="_blank">
+				<div className="clear-button" onMouseEnter={() => setEmailHover(true)}
+				onMouseLeave={() => setEmailHover(false)}>
+					<svg>
+						<rect x="0" y="0" fill="none" width="100%" height="100%" />
+					</svg>
+					{ emailHover ? "eddiewchong@outlook.com" : "email"}
+				</div>
+			</a>
 			<Button text="Projects" href="https://github.com/ewkchong" />
 			<Button text="LinkedIn" href="https://linkedin.com/in/ewkchong" />
 			<Button text="Resume" href="/eddieresume.pdf" />
