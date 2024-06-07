bb4cb1d6f141d833a652c59d86779004431227d1
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2024-06-07 00:41:58 -0700

    fix: add WIP placeholders

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,14 @@
 import { AnimatePage } from "../components/AnimatePage";
 
 export const metadata = {
-	title: "Projects - Edward Chong"
+	title: "Contact - Edward Chong"
 }
 
 export default function Projects() {
 	return (
 		<AnimatePage>
 			<main>
-				<h1 className="title-header">Projects - WIP</h1>
+				<h1 className="title-header">Contact Me - WIP</h1>
 				<p className="mt-8">Coming soon...</p>
 			</main>
 		</AnimatePage>
